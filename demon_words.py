f6539e79df3570d08b72331a23fa5bcb2e43e045
DJ Knox <dhknox@gmail.com>
2019-06-09 16:23:36 -0400

    refactor functions in normal mode to be used in evil mode - evil mode still WIP

--- demon_words.py
+++ demon_words.py
@@ -1,54 +1,134 @@
+# import random module for randomly selecting a word length
+import random
+
+# import get_terminal_size function from os to center console output
+from os import get_terminal_size
+
+# import reusable functions from mystery word game
+from mystery_word import print_difficulty_setting_instructions, ask_user_for_difficulty_setting, load_words, group_words_by_difficulty, choose_list_of_words_based_on_difficulty_setting, ask_user_to_guess_a_letter, play_round_of_game
+
 # In Evil Hangman, the computer maintains a list of every word in the English language, then continuously pares down the word list to try to dodge the player's guesses as much as possible.
 # The computer begins by maintaining a list of all words in the English language of a particular length.
 # Whenever the player guesses, the computer partitions the words into "word families" based on the positions of the guessed letters in the words.
 # Once the words are partitioned into equivalence classes, the computer can pick the largest of these classes to use as its remaining word list.
 # It then reveals the letters in the positions indicated by the word family.
+def evil_algorithm(word_list):
+    """
+    Given a list of words, continuously pare down the list of words into groups based on user letter guesses.
+    Groups are based on the position/index of the guessed letter in the words and only the largest group of words will remain for subsequent guesses.
+    When the list of words has been pared down to a single word, return that word.
+    Return a list of the user's guesses.
+    """
+    guesses = []
+    while len(word_list) > 0:
+        if len(word_list) == 1:
+            return word_list[0], guesses
 
+        # ask user to guess a letter
+        guess = ask_user_to_guess_a_letter()
+        guesses.append(guess)
 
-# user guesses a letter
-# loop through each word and create word families based on where the guessed letter exists in the word
-# word_families = {
-#     0: ['art'],
-#     1: ['bars', 'cat', 'tart', 'bat'],
-#     2: ['crab']
-# }
-word_list = ['art', 'bars', 'cat', 'crab', 'tart', 'bat', 'touch', 'match', 'matches']
+        # loop through each word and create word families based on where the guessed letter exists in the word
+        # word_families = {
+        #     0: ['art'],                           --> 'a' is in first index
+        #     1: ['bars', 'cat', 'tart', 'bat'],    --> 'a' is in second index
+        #     2: ['crab']                           --> 'a' is in third index
+        # }
+        word_families = {}
+        for word in word_list:
+            for i in range(0, len(word)):
+                if guess == word[i]:
+                    if i in word_families.keys():
+                        word_families[i].append(word)
+                    else:
+                        word_families[i] = [word]
 
-while len(word_list) > 0:
-    if len(word_list) == 1:
-        # play normal game with new word
-        print("now play normal game with", word_list[0])
-        break
+        # loop through word_families and pick the largest family as new word list
+        word_list_lengths = []
+        for key in word_families:
+            word_list_lengths.append(len(word_families[key]))
 
-    # ask user for guess
-    guess = input("Guess a letter: ")
-
-    # create word families based on guess
-    word_families = {}
-    for word in word_list:
-        for i in range(0, len(word)):
-            if guess == word[i]:
-                if i in word_families.keys():
-                    word_families[i].append(word)
-                else:
-                    word_families[i] = [word]
-
-    print(f"Word Families created: {word_families}")
-
-    # loop through word_families and pick the largest family as new word list
-    # loop through each key in word_families
-    # get the number of elements in the key's value
-    # return the key with the most elements in its value
-    # return the value with the max number of elements
-    word_list_lengths = []
-    for key in word_families:
-        word_list_lengths.append(len(word_families[key]))
-
-    for word_family in word_families.values():
-        if len(word_family) == max(word_list_lengths):
-            word_list = word_family
-
-    print(f"New Word list: {word_list}")
+        for word_family in word_families.values():
+            if len(word_family) == max(word_list_lengths):
+                word_list = word_family
+        
 
 
 
+if __name__ == "__main__":
+    replay_game = True
+    while replay_game:
+        # print centered greeting and difficulty settings to user
+        print(f"{'Welcome to the Mystery Word game!':^{get_terminal_size().columns}}")
+        print_difficulty_setting_instructions()
+
+        # ask user for difficulty setting or to quit game
+        option = ask_user_for_difficulty_setting()
+
+        # exit the loop if user selects 'x'
+        if option == 'x':
+            print(f"{'Quitting game!':^{get_terminal_size().columns}}")
+            break
+        
+        # load the words from the text file
+        words = load_words()
+
+        # sort the words based on their difficulty
+        words_grouped_by_difficulty = group_words_by_difficulty(words)
+
+        # take the user's choice of difficulty and choose a list based on the user's option
+        chosen_list = choose_list_of_words_based_on_difficulty_setting(words_grouped_by_difficulty, option)
+
+        # take random word length (within the chosen difficulty) and create new list of words with only those lengths
+        lengths_of_words_in_chosen_list = [len(word) for word in chosen_list]
+        random_word_length = random.choice(lengths_of_words_in_chosen_list)
+        chosen_list = [word for word in chosen_list if len(word) == random_word_length]
+
+        # lie to user and tell them a word has been chosen, but be truthful about length of word that will eventually be chosen
+        word_length_message = f"The chosen word has {random_word_length} letters."
+        print(f"{word_length_message:^{get_terminal_size().columns}}")
+
+        # take the chosen_list and feed into evil algorithm to get word to play with
+        # get the user's guesses from the evil algorithm round of guessing
+        chosen_word, guesses = evil_algorithm(chosen_list)
+
+        # after evil algorithm chooses a final word, play game normally with variables updated after evil round
+        # in order to make letter casing not matter, make chosen_word lowercase
+        # will also make all inputted guesses lowercase and check against chosen_word_lowercase
+        chosen_word_lowercase = chosen_word.lower()
+
+        # A user is allowed 8 guesses. Remind the user of how many guesses they have left after each round.
+        # initialize the game variables
+        num_guesses_allowed = 8
+        num_guesses = len(guesses)
+        guesses = guesses
+        letters_left_to_guess = [letter for letter in list(chosen_word_lowercase) if letter not in guesses]
+
+        # The game should end when the user constructs the full word or runs out of guesses.
+        while num_guesses < num_guesses_allowed and len(letters_left_to_guess) > 0:
+            # play a round of the game and update variables each time
+            num_guesses, guesses, letters_left_to_guess = play_round_of_game(num_guesses, num_guesses_allowed, guesses, letters_left_to_guess, chosen_word_lowercase)
+
+        # If the player runs out of guesses, reveal the word to the user when the game ends.
+        if len(letters_left_to_guess) == 0:
+            print(f"{'You won!':^{get_terminal_size().columns}}")
+        else:
+            print(f"{'You lost!':^{get_terminal_size().columns}}")
+
+        chosen_word_message = f"The word was {chosen_word}!"
+        print(f"{chosen_word_message:^{get_terminal_size().columns}}")
+
+        # When a game ends, ask the user if they want to play again. The game begins again if they reply positively.
+        print("Enter '1' to play again and anything else to quit the game.")
+        does_user_want_to_play_again = input("Play again? ")
+        if does_user_want_to_play_again == '1':
+            print("Let's play again!")
+        else:
+            print(f"{'Thanks for playing!':^{get_terminal_size().columns}}")
+            replay_game = False
+
+# TODO:
+# while in evil mode guessing rounds:
+# figure out way to display partially guessed "word" each time
+# since the word changes with each round, display the correctly guessed letters in their proper indices
+# display "_" for the remaining indices
--- mystery_word.py
+++ mystery_word.py
@@ -80,6 +80,40 @@ def choose_list_of_words_based_on_difficulty_setting(words_grouped_by_difficulty
     
     return chosen_list
 
+def ask_user_to_guess_a_letter():
+    """
+    Ask the user to guess a letter and return their guess.
+    """
+    valid_input = False
+    while not valid_input:
+        # ask user to guess a letter and immediately make their guess lowercase
+        guess = input("Guess a letter! ").lower()
+        # if guess is more than one letter or not a letter
+        if len(guess) > 1 or not guess.isalpha():
+            print("Input invalid. Guess again.")
+        else:
+            valid_input = True
+    return guess
+
+def check_guess(guess, guesses, chosen_word, num_guesses, letters_left_to_guess):
+    """"
+    Given the user's guess, the list of guesses already made, the word being guessed, the number of guesses made, and the letters left to guess, validate the guess and update the list of guesses, the number of guesses, and letters left to guess.
+    """
+    if guess in guesses:
+        print("You've already guessed that letter!")
+    elif guess in chosen_word:
+        print("That letter is in the word!")
+        # add guess to guesses
+        guesses.append(guess)
+        # remove all occurrences of correct guess from letters_left_to_guess
+        letters_left_to_guess = [letter for letter in letters_left_to_guess if letter != guess]
+    elif guess not in chosen_word:
+        print("That letter is not in the word!")
+        num_guesses += 1
+        guesses.append(guess)
+    
+    return guesses, num_guesses, letters_left_to_guess
+
 def display_num_guesses_remaining(num_guesses_allowed, num_guesses):
     """
     Given an integer for the number of guesses allowed and an integer for the number of guesses made, print out a string telling the user how many guesses they have remaining.
@@ -90,22 +124,49 @@ def display_num_guesses_remaining(num_guesses_allowed, num_guesses):
     elif num_guesses_remaining == 1:
         print(f"You have {num_guesses_remaining} guess left!")
 
+def play_round_of_game(num_guesses, num_guesses_allowed, guesses, letters_left_to_guess, chosen_word):
+    """
+    Given a number of guesses, number of guesses allowed, a list of guesses, the letters left to guess, and the chosen word, play a round of the mystery word game.
+    Update the number of guesses, the list of guesses, and the letters left to guess.
+    """
+    # Display the partially guessed word, as well as letters that have not been guessed.
+    # create list of letters that have been guessed and underscores for those that have not
+    partially_guessed_word = [letter if letter in guesses else "_" for letter in chosen_word.lower()]
+    # collapse list into a string separated by spaces for formatting
+    partially_guessed_word = ' '.join(partially_guessed_word)
+    print(f"{partially_guessed_word:^{get_terminal_size().columns}}")
+
+    # Ask the user to supply one guess (i.e. letter) per round. This letter can be upper or lower case and it should not matter. If a user enters more than one letter, tell them the input is invalid and let them try again.
+    guess = ask_user_to_guess_a_letter()
+
+    # Let the user know if their guess appears in the computer's word.
+    # A user loses a guess only when they guess incorrectly. If they guess a letter that is in the computer's word, they do not lose a guess.
+    # If the user guesses the same letter twice, do not take away a guess. Instead, print a message letting them know they've already guessed that letter and ask them to try again.
+    # check the user's guess and update guesses, num_guesses, and letters_left_to_guess
+    guesses, num_guesses, letters_left_to_guess = check_guess(guess, guesses, chosen_word.lower(), num_guesses, letters_left_to_guess)
+    
+    # tell the user how many guesses they have left
+    display_num_guesses_remaining(num_guesses_allowed, num_guesses)
+
+    # show the user a list of the letters they have already guessed
+    letters_guessed_message = f"Letters guessed: {guesses}"
+    print(f"{letters_guessed_message:^{get_terminal_size().columns}}")
+
+    return num_guesses, guesses, letters_left_to_guess
+
 if __name__ == "__main__":
     replay_game = True
     while replay_game:
-        # terminal_width will be used for centering console output
-        terminal_width = get_terminal_size().columns
-
         # print centered greeting and difficulty settings to user
-        print(f"{'Welcome to the Mystery Word game!':^{terminal_width}}")
+        print(f"{'Welcome to the Mystery Word game!':^{get_terminal_size().columns}}")
         print_difficulty_setting_instructions()
 
         # ask user for difficulty setting or to quit game
         option = ask_user_for_difficulty_setting()
 
-        # exit the loop of user selects 'x'
+        # exit the loop if user selects 'x'
         if option == 'x':
-            print(f"{'Quitting game!':^{terminal_width}}")
+            print(f"{'Quitting game!':^{get_terminal_size().columns}}")
             break
 
         # load the words from the text file
@@ -120,13 +181,14 @@ if __name__ == "__main__":
         # choose a random word from the chosen list and tell the user its length
         chosen_word = random.choice(chosen_list)
         word_length_message = f"The chosen word has {len(chosen_word)} letters."
-        print(f"{word_length_message:^{terminal_width}}")
+        print(f"{word_length_message:^{get_terminal_size().columns}}")
 
         # in order to make letter casing not matter, make chosen_word lowercase
         # will also make all inputted guesses lowercase and check against chosen_word_lowercase
         chosen_word_lowercase = chosen_word.lower()
 
         # A user is allowed 8 guesses. Remind the user of how many guesses they have left after each round.
+        # initialize the game variables
         num_guesses_allowed = 8
         num_guesses = 0
         guesses = []
@@ -134,56 +196,17 @@ if __name__ == "__main__":
 
         # The game should end when the user constructs the full word or runs out of guesses.
         while num_guesses < num_guesses_allowed and len(letters_left_to_guess) > 0:
+            # play a round of the game and update variables each time
+            num_guesses, guesses, letters_left_to_guess = play_round_of_game(num_guesses, num_guesses_allowed, guesses, letters_left_to_guess, chosen_word_lowercase)
 
-            # Display the partially guessed word, as well as letters that have not been guessed.
-            # create list of letters that have been guessed and underscores for those that have not
-            partially_guessed_word = [letter if letter in guesses else "_" for letter in chosen_word_lowercase]
-            # collapse list into a string separated by spaces for formatting
-            partially_guessed_word = ' '.join(partially_guessed_word)
-            print(f"{partially_guessed_word:^{terminal_width}}")
-
-            # Ask the user to supply one guess (i.e. letter) per round. This letter can be upper or lower case and it should not matter. If a user enters more than one letter, tell them the input is invalid and let them try again.
-            valid_input = False
-            while not valid_input:
-                # ask user to guess a letter and immediately make their guess lowercase
-                guess = input("Guess a letter! ").lower()
-                # if guess is more than one letter or not a letter
-                if len(guess) > 1 or not guess.isalpha():
-                    print("Input invalid. Guess again.")
-                else:
-                    valid_input = True
-
-            # Let the user know if their guess appears in the computer's word.
-            # A user loses a guess only when they guess incorrectly. If they guess a letter that is in the computer's word, they do not lose a guess.
-            # If the user guesses the same letter twice, do not take away a guess. Instead, print a message letting them know they've already guessed that letter and ask them to try again.
-            if guess in guesses:
-                print("You've already guessed that letter!")
-            elif guess in chosen_word_lowercase:
-                print("That letter is in the word!")
-                # add guess to guesses
-                guesses.append(guess)
-                # remove all occurrences of correct guess from letters_left_to_guess
-                letters_left_to_guess = [letter for letter in letters_left_to_guess if letter != guess]
-            elif guess not in chosen_word_lowercase:
-                print("That letter is not in the word!")
-                num_guesses += 1
-                guesses.append(guess)
-            
-            # tell the user how many guesses they have left
-            display_num_guesses_remaining(num_guesses_allowed, num_guesses)
-
-            # show the user a list of the letters they have already guessed
-            letters_guessed_message = f"Letters guessed: {guesses}"
-            print(f"{letters_guessed_message:^{terminal_width}}")
-        
         # If the player runs out of guesses, reveal the word to the user when the game ends.
         if len(letters_left_to_guess) == 0:
-            print(f"{'You won!':^{terminal_width}}")
+            print(f"{'You won!':^{get_terminal_size().columns}}")
         else:
-            print(f"{'You lost!':^{terminal_width}}")
+            print(f"{'You lost!':^{get_terminal_size().columns}}")
 
         chosen_word_message = f"The word was {chosen_word}!"
-        print(f"{chosen_word_message:^{terminal_width}}")
+        print(f"{chosen_word_message:^{get_terminal_size().columns}}")
 
         # When a game ends, ask the user if they want to play again. The game begins again if they reply positively.
         print("Enter '1' to play again and anything else to quit the game.")
@@ -191,5 +214,5 @@ if __name__ == "__main__":
         if does_user_want_to_play_again == '1':
             print("Let's play again!")
         else:
-            print(f"{'Thanks for playing!':^{terminal_width}}")
+            print(f"{'Thanks for playing!':^{get_terminal_size().columns}}")
             replay_game = False
